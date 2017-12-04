a2809a23379bb15e78984a1141f5109897c7d8c5
Daniel Ignat <daniel.ignat@ica.se>
2017-12-04 21:50:58 +0100

    Add failing test where collision detection too sensitive

---
+++ transforms/__testfixtures__/no-iffe-for-module-exports/iffe-unwrap-4.input.js
@@ -0,0 +1,12 @@
+module.exports = (function () {
+  function innerFunction1 () {
+    const a = 1
+    return a
+  }
+  function innerFunction2 () {
+    const b = 1
+    return b
+  }
+
+  return { innerFunction1, innerFunction2 }
+}())
